d2b89367505722d3f1caf459d245bc12cb965e1d
Gary McDaniel <gary_mcdaniel@comcast.net>
2018-08-12 19:00:34 -0600

    added CSS to .html

--- pyramid.js
+++ pyramid.js
@@ -1,5 +1,6 @@
 var heightElem = document.getElementById("height");
 var formElem = document.getElementById("draw-form");
+var shapeElem = document.getElementById("shape");
 
 formElem.oninput = function(event) {
 
@@ -20,17 +21,17 @@ function drawPyramid(height) {
         // figure out number of bricks and spaces
         var numBricks = row + 2;
         var numSpaces = height - row - 1;
-        
+        var rowStr = ""
         // build up a string for this row
-        var rowStr = "";
+        
         for (var i = 0; i < numSpaces; i++) {
             var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
             rowStr += spaceChar;
         }
         for (var i = 0; i < numBricks; i++) {
-            rowStr += "Y";
+            rowStr += "@";
         }
-        console.log(rowStr)
+        
         rowElem = document.createElement("p");
         rowElem.innerHTML = rowStr;
         document.getElementById("pyramid").appendChild(rowElem);
